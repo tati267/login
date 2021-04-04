a0fbbebd8aa1e190d3d7a7b9ec720cbd20e0a4d2
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-04-04 10:07:30 +1000

    Add inputs validation,inputs error

--- src/js/helpers/validate.js
+++ src/js/helpers/validate.js
@@ -2,3 +2,15 @@ const regExpDic = {
     email: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
     password: /^[0-9a-zA-Z]{4,}$/,
 }
+
+/*
+Function validate. Check Input on RegExp provided in regExpDic by input data-required type
+@param {htmlInputElement} el
+@returns {Boolean} -Returns true if input valid or won't have data required attribute
+*/
+
+export function validate(el) {
+    const regExpName = el.dataset.required;
+    if (!regExpDic[regExpName]) return true;
+    return regExpDic[regExpName].test(el.value)
+}
