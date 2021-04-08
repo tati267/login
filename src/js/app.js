248069d646ed17eb8106f26a5310eef4800f0703
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-04-08 12:03:22 +1000

    Add interceptors, token

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import { validate } from './helpers/validate';
 import { showInputError, removeInputError } from './views/form';
 import { login } from './services/auth.service';
 import { notify } from './views/notification';
+import { getNews} from './services/news.service';
 
 const { form, inputEmail, inputPassword } = UI;
 const inputs = [inputEmail, inputPassword];
@@ -33,6 +34,7 @@ async function onSubmit() {
 
 	try {
 		await login(inputEmail.value, inputPassword.value);
+		await getNews();
 		form.reset();
 		//show success notify
 		notify({ mas: "Login success", className: "alert-success" })
--- src/js/services/auth.service.js
+++ src/js/services/auth.service.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
-import API_ENV from '../config/api.config';
-
+import axios from '../plugins/axios';
 
 /*
 Function login. Make login request to API
@@ -10,13 +8,9 @@ Function login. Make login request to API
 export async function login(email, password) {
     try {
         const response = await axios.post(
-            `${API_ENV.apiUrl}/auth/login`, 
+            `/auth/login`,
             JSON.stringify({ email, password }),
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
+        );
         console.log(response);
         return response.data;
     } catch (err) {
